abbb0e0029439a81cd974898863ed48bad0176bb
AnanyaKanwar <kanwarananya93@gmail.com>
2024-03-05 15:49:39 +0530

    .

---
+++ tailwind.config.js
@@ -0,0 +1,15 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: ["*"],
+  theme: {
+    extend: {
+      colors:{
+        bodyColor:'#fffaeb',
+        textColor:'#F9B800'
+      }
+
+    },
+  },
+  plugins: [],
+}
+
